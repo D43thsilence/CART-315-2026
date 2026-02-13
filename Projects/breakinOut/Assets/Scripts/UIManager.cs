84bc00a4de85d5aad50c2d78a5240b9d82cfd748
D43thsilence <malcolm.tadonki@gmail.com>
2026-02-12 19:25:29 -0500

    Improved the Schmup scene and added a start screen to the breakinOut program

--- Projects/breakinOut/Assets/Scripts/UIManager.cs
+++ Projects/breakinOut/Assets/Scripts/UIManager.cs
@@ -1,22 +1,31 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
-public class UIManager : MonoBehaviour {
+public class UIManager : MonoBehaviour
+{
     public TMP_Text finalScore;
     public TMP_Text highScore;
 
     public int number1;
     private int number2;
-    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start() {
+    void Start()
+    {
         ShowScore();
     }
 
     // Update is called once per frame
-    void ShowScore() {
+    void ShowScore()
+    {
         finalScore.text = "Final Score:" + GameManager.S.points;
-        
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Shmup");
     }
 
     // public string GetName() {
